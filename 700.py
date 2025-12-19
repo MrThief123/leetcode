730a081746306e7a3248369579131cbbd505b317
MrThief123 <90836480+MrThief123@users.noreply.github.com>
2025-12-19 16:40:05 +1100

    Simple BST 700

---
+++ 700.py
@@ -0,0 +1,22 @@
+'''
+Search for a value in a Binary Search Tree
+Utilize the properties of BST to traverse left or right based on comparisons
+Return the node if found, otherwise return None'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        while root and root.val != val:
+            if root.val < val:
+                root = root.right 
+            else:
+                root = root.left
+
+        return root
+
+        
