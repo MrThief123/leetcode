2476da5ec57e702672049f66558dabb942ac78f5
MrThief123 <90836480+MrThief123@users.noreply.github.com>
2026-01-25 23:28:15 +1100

    189

---
+++ 189.py
@@ -0,0 +1,16 @@
+'''
+169. Majority Element
+'''
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        temp = [0 for i in range(n)]
+        for i in range(n):
+            temp[(i+k)%n] = nums[i]
+
+        for i in range(n):
+            nums[i] = temp[i]
