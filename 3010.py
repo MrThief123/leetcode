27a6ff046ba190d15d0ec7b9a69f360055cecac1
MrThief123 <90836480+MrThief123@users.noreply.github.com>
2026-02-01 20:47:02 +1100

    3010

---
+++ 3010.py
@@ -0,0 +1,20 @@
+'''
+3010. Minimum Cost to Make Array Equal
+Easy
+Find the sum of the smallest and second smallest elements plus the first element.
+'''
+
+class Solution:
+    def minimumCost(self, nums: List[int]) -> int:
+        cost = nums[0]
+
+        min1, min2 = float('inf'), float('inf')
+
+        for num in nums[1:]:
+            if num <= min1:
+                min2 = min1
+                min1 = num
+            elif num <= min2:
+                min2 = num
+
+        return cost + min1 + min2
